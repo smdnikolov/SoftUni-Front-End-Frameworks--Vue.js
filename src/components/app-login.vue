59349f1d67eb838c32f79a8db50c0a2d871ac7fb
smdnikolov <smdnikolov@gmail.com>
2020-04-06 20:51:08 +0300

    Update app-login.vue
    Added validation to the form in the respective componenet

--- src/components/app-login.vue
+++ src/components/app-login.vue
@@ -6,17 +6,47 @@
         <div class="container">
           <h1>Login</h1>
           <div class="email">
-            <input type="email" v-model="email" placeholder="email" />
+            <input
+              type="email"
+              v-model="email"
+              placeholder="email"
+              v-model.trim="$v.email.$model"
+              :class="{'is-invalid':$v.email.$error}"
+            />
+            <div class="invalid-feedback">
+              <span v-if="!$v.email.email && this.email !==''">The email should be in valid format.</span>
+            </div>
           </div>
           <div class="password">
-            <input type="password" v-model="password" placeholder="password" />
+            <input
+              type="password"
+              v-model="password"
+              placeholder="password"
+              v-model.trim="$v.password.$model"
+              :class="{'is-invalid':$v.password.$error}"
+            />
+            <div class="invalid-feedback">
+              <span
+                v-if="!$v.password.password && this.password !==''"
+              >The password sholud have at least 6 characters, one number and one special character.</span>
+            </div>
           </div>
-          <button type="submit" class="login">Login</button>
+          <button
+            :disabled="$v.email.$error || $v.password.$error || (this.password === '' || this.email ==='')"
+            type="submit"
+            class="login"
+          >Login</button>
         </div>
         <div style="color:#f5860a">
           Don't have an account ?
           <div div style="color:#f5860a;padding:10px">
             <router-link class="button" to="/register">Register</router-link>
+            <div class="invalid">
+              <span
+                style="margin-top:10px"
+                v-if="$v.password.$error || $v.email.$error || (this.password === '' || this.email ==='')"
+              >Please fill the form correclty.</span>
+            </div>
           </div>
         </div>
       </form>
@@ -27,6 +57,7 @@
 <script>
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   data() {
@@ -35,6 +66,20 @@ export default {
       password: "",
       error: ""
     };
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      password(password) {
+        return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(
+          password
+        );
+      }
+    }
   },
   methods: {
     logIn() {
@@ -53,6 +98,28 @@ export default {
 </script>
 
 <style>
+button[disabled],
+html input[disabled] {
+  cursor: not-allowed;
+}
+.is-invalid {
+  color: red !important;
+  outline-color: red !important;
+  border-color: red !important;
+  background: radial-gradient(
+    rgba(201, 22, 22, 0.1),
+    rgba(245, 108, 108, 0.15)
+  );
+}
+.invalid-feedback {
+  text-align: left;
+  color: red;
+}
+.invalid {
+  text-align: center;
+  color: gray;
+  margin-top: 20px;
+}
 .error {
   color: red;
 }
