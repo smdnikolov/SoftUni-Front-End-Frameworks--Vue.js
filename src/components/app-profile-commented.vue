973ada69bce3d85c90dd3cc096f3e236a71c07da
smdnikolov <smdnikolov@gmail.com>
2020-04-07 17:55:15 +0300

    Update app-profile-commented.vue
    Minor typo fix

--- src/components/app-profile-commented.vue
+++ src/components/app-profile-commented.vue
@@ -5,7 +5,7 @@
       <h2>You have not commented on any memes yet. Browse through the categories, enjoy and comment.</h2>
     </div>
     <div v-else>
-      <h2>Created Memes</h2>
+      <h2>Commented</h2>
     </div>
     <div class="row row2">
       <div v-if="loading" class="col-sm-4 center">
